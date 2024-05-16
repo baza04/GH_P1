97f1685780712d4fbb2c270377d3a0393c6aebf0
Amir Bazarbay <abazarbay@ozon.ru>
2024-05-16 22:42:39 +0500

    start p1

---
+++ code/ReadFile.java
@@ -0,0 +1,11 @@
+import java.io.IOExcepption;
+import java.path.Path;
+
+
+public class ReadFile {
+
+    public static void main(String[] args) {
+        Path path = Paths.get("example_path.txt");
+
+    }
+}
